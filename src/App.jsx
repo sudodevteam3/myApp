8c5ab7168df81bad162a4e7c3676d9a4cacb95d2
sudodevteam3 <team.anandsb333@gmail.com>
2025-02-19 00:58:49 -0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+import Contact from '../pages/Contact';
+import About from '../pages/About';
+// import ErrorPage from './pages/ErrorPage'; // Add an error page component
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+
+    </> // Add a catch-all route for error handling
+  )
+)
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
