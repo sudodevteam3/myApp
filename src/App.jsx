c25c81996cc035339227ab6b9788b4bf52895a08
sudodevteam3 <team.anandsb333@gmail.com>
2025-02-19 01:12:04 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-      <Route path="contact" element={<Contact />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
 
     </> // Add a catch-all route for error handling
   )
